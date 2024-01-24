0b78b0a3f26028e906edfc39f8fc2da02a3b552e
nahid <nahidrabbi045@gmail.com>
2024-01-24 16:01:09 +0600

    projects done

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,4 @@ module.exports = {
     plugins: [],
   }
 
-  module.exports = {
-    theme: {
-      extend: {
-        backgroundImage: {
-          'hero-pattern': "url('/img/hero-pattern.svg')",
-          'footer-texture': "url('/img/footer-texture.png')",
-        }
-      }
-    }
-  }
+  
